9fda12afee7c2f4fb9fd2cd4c0bdb00841ca07e9
caiges <caigesn@gmail.com>
2018-04-16 18:58:30 -0700

    Create target dirs after checkout

--- ci/stage.sh
+++ ci/stage.sh
@@ -6,11 +6,11 @@ BRANCH=$1
 
 rm -rf version/$BRANCH
 rm -rf swagger
-mkdir -p version/$BRANCH
-mkdir -p swagger
 GIT_SSH_COMMAND='ssh -i /var/lib/jenkins/.ssh/velopaymentsopenapi' git fetch git@github.com:velopaymentsapi/VeloOpenApi.git gh-pages
 git checkout gh-pages
 GIT_SSH_COMMAND='ssh -i /var/lib/jenkins/.ssh/velopaymentsopenapi' git pull
+mkdir -p version/$BRANCH
+mkdir -p swagger
 cp -R tmp/redoc/* version/$BRANCH/
 cp -R tmp/swagger/* swagger/
 
