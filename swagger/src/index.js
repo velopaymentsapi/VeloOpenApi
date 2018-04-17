2ac06f9898371f1b9c2717a237642536a3fa9761
caiges <caigesn@gmail.com>
2018-04-16 19:02:57 -0700

    Looks like version is reserved for github

--- swagger/src/index.js
+++ swagger/src/index.js
@@ -4,8 +4,8 @@ import 'swagger-ui/dist/swagger-ui.css';
 
 const url = new URL(window.location);
 const params = new URLSearchParams(url.search);
-const version = params.get("version") || "master";
-const specURL = `https://velopaymentsapi.github.io/VeloOpenApi/version/${version}/openapi.yaml`
+const branch = params.get("branch") || "master";
+const specURL = `https://velopaymentsapi.github.io/VeloOpenApi/version/${branch}/openapi.yaml`
 
 SwaggerUI({
   dom_id: '#root',
